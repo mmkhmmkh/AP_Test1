69b15bf6ece15018502df9e8dabe8fdb79ce656d
MMKH <vbha.mmk@gmail.com>
2022-03-07 18:02:56 +0330

    Test1

--- src/ir/aut/test1/Main.java
+++ src/ir/aut/test1/Main.java
@@ -1,23 +1,41 @@
 package ir.aut.test1;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Student std1 = new Student("Mahdi", "Khancherli", "1234567");
-        Student std2 = new Student("Mohammad", "Noormohammadi", "7654321");
 
-        std1.setGrade(15);
-        std2.setGrade(20);
+        ArrayList<String> arr = new ArrayList<>();
 
-        System.out.println(std1);
-        System.out.println(std2);
+        arr.add("A abc");
+        arr.add("B abc");
+        arr.add("C abc");
+        arr.add("D abc");
+        arr.add("E abc");
+        arr.add("F abc");
 
-        Lab lab = new Lab("MMKH", "Monday", 3);
 
-        lab.enrollStudent(std1);
-        lab.enrollStudent(std2);
 
-        lab.printLabInfo();
+        System.out.println(arr);
+
+
+//        Student std1 = new Student("Mahdi", "Khancherli", "1234567");
+//        Student std2 = new Student("Mohammad", "Noormohammadi", "7654321");
+//
+//        std1.setGrade(15);
+//        std2.setGrade(20);
+//
+//        System.out.println(std1);
+//        System.out.println(std2);
+//
+//        Lab lab = new Lab("MMKH", "Monday", 3);
+//
+//        lab.enrollStudent(std1);
+//        lab.enrollStudent(std2);
+//
+//        lab.printLabInfo();
     }
 }
